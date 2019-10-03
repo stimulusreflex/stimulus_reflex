93eeaaf6f4d331a98382dbe0f670dea30b84900f
Nathan Hopkins <natehop@gmail.com>
2019-10-03 09:56:16 -0600

    Stricter handling of attribute parsing

--- javascript/attributes.js
+++ javascript/attributes.js
@@ -1,19 +1,32 @@
 import { dasherize, underscore } from 'inflected'
 
-// Returns the expected matching controller name for the passed reflex.
+// Returns a string value for the passed array.
 //
-// Example
+//   attributeValue(['', 'one', null, 'two', 'three ']) // 'one two three'
 //
-//   matchingControllerName('ExampleReflex#do_stuff') // -> 'example'
-//
-export const matchingControllerName = reflex => {
-  return dasherize(underscore(reflex.split('#')[0].replace(/Reflex$/, '')))
-}
-
 export const attributeValue = (values = []) => {
   const value = values
     .filter(v => v && String(v).length)
+    .map(v => v.trim())
     .join(' ')
     .trim()
   return value.length ? value : null
+}
+
+// Returns an array for the passed string value by splitting on whitespace.
+//
+//   attributeValues('one two three ') // ['one', 'two', 'three']
+//
+export const attributeValues = value => {
+  if (!value) return []
+  if (!value.length) return []
+  return value.split(' ').filter(v => v.trim().length)
+}
+
+// Returns the expected matching controller name for the passed reflex.
+//
+//   matchingControllerName('ExampleReflex#do_stuff') // 'example'
+//
+export const matchingControllerName = reflex => {
+  return dasherize(underscore(reflex.split('#')[0].replace(/Reflex$/, '')))
 }
--- javascript/test/attributes.test.js
+++ javascript/test/attributes.test.js
@@ -1,36 +1,71 @@
 import assert from 'better-assert'
-import { attributeValue, matchingControllerName } from '../attributes'
+import * as attrs from '../attributes'
 
 describe('attributeValue', () => {
-  it('should return empty string when no arguments passed', () => {
-    assert(attributeValue() === null)
+  it('returns empty string for no arguments', () => {
+    assert(attrs.attributeValue() === null)
   })
 
-  it('should return empty string when empty array passed', () => {
-    assert(attributeValue([]) === null)
+  it('returns empty string for empty array', () => {
+    assert(attrs.attributeValue([]) === null)
   })
 
-  it('should return correct value when array with single value passed', () => {
-    assert(attributeValue(['example']) === 'example')
+  it('returns correct value for array with single value', () => {
+    assert(attrs.attributeValue(['example']) === 'example')
   })
 
-  it('should return correct value when array with multiple values passed', () => {
-    assert(attributeValue(['one', 'two', 'three']) === 'one two three')
+  it('returns correct value for array with multiple values', () => {
+    assert(attrs.attributeValue(['one', 'two', 'three']) === 'one two three')
   })
 
-  it('should return correct value when array with mixed values passed', () => {
+  it('returns correct value for array with multiple values that include whitespace', () => {
     assert(
-      attributeValue(['one', '', 'two', 'three', null]) === 'one two three'
+      attrs.attributeValue([' one ', 'two ', 'three ']) === 'one two three'
+    )
+  })
+
+  it('returns correct value for array with mixed values', () => {
+    assert(
+      attrs.attributeValue(['one', '', 'two', 'three', null]) ===
+        'one two three'
     )
   })
 })
 
-describe('matchingControllerName', () => {
-  it('should return correct name for reflex', () => {
-    assert(matchingControllerName('ExampleReflex#work') === 'example')
+describe('attributeValues', () => {
+  it('returns empty array for no arguments', () => {
+    assert(attrs.attributeValues().length === 0)
   })
 
-  it('should return correct name abbreviated for event + reflex', () => {
-    assert(matchingControllerName('Example#work') === 'example')
+  it('returns correct array for single value', () => {
+    const list = attrs.attributeValues('one')
+    assert(list.length === 1)
+    assert(list[0] === 'one')
+  })
+
+  it('returns correct array for multiple values', () => {
+    const list = attrs.attributeValues('one two three')
+    assert(list.length === 3)
+    assert(list[0] === 'one')
+    assert(list[1] === 'two')
+    assert(list[2] === 'three')
+  })
+
+  it('returns correct array for multiple values with whitespace padding', () => {
+    const list = attrs.attributeValues(' one two   three ')
+    assert(list.length === 3)
+    assert(list[0] === 'one')
+    assert(list[1] === 'two')
+    assert(list[2] === 'three')
+  })
+})
+
+describe('matchingControllerName', () => {
+  it('returns correct name for reflex', () => {
+    assert(attrs.matchingControllerName('ExampleReflex#work') === 'example')
+  })
+
+  it('returns correct name abbreviated for event + reflex', () => {
+    assert(attrs.matchingControllerName('Example#work') === 'example')
   })
 })
