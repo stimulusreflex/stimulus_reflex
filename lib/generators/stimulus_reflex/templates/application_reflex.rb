c561c6afbfe559958db6ca692cc432c527232be5
Nathan Hopkins <natehop@gmail.com>
2019-10-11 14:56:54 -0600

    Update templates with helpful comments

--- lib/generators/stimulus_reflex/templates/application_reflex.rb
+++ lib/generators/stimulus_reflex/templates/application_reflex.rb
@@ -1,2 +1,8 @@
 class ApplicationReflex < StimulusReflex::Reflex
+  # Add application wide Reflex behavior in this file.
+  #
+  # Example:
+  #
+  #   # If your ActionCable connection is: `identified_by :current_user`
+  #   delegate :current_user, to: :channel
 end
--- lib/generators/stimulus_reflex/templates/custom_reflex.rb
+++ lib/generators/stimulus_reflex/templates/custom_reflex.rb
@@ -1,2 +1,19 @@
 class <%= @name.classify %>Reflex < ApplicationReflex
+  # Add Reflex methods in this file.
+  #
+  #   All Reflex instances expose the following properties.
+  #
+  #   - connection - the ActionCable connection
+  #   - channel - the ActionCable channel
+  #   - request - an ActionDispatch::Request proxy for the socket connection
+  #   - session - the ActionDispatch::Session store for the current visitor
+  #   - url - the URL of the page that triggered the reflex
+  #   - element - a Hash like object that represents the HTML element that triggered the reflex
+  #
+  # Example:
+  #
+  #   def example(argument=true)
+  #     # Your logic here...
+  #     # Any declared instance variables will be made available to the Rails controller and view.
+  #   end
 end
