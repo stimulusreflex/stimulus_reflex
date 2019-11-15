c5cdeb4a9457ac47bffe15fe689dd7b42b647556
szTheory <szTheory@users.noreply.github.com>
2019-11-15 01:36:17 -0500

    Add the actual error message before the stacktrace. Method refactor.

--- lib/stimulus_reflex/channel.rb
+++ lib/stimulus_reflex/channel.rb
@@ -32,14 +32,14 @@ class StimulusReflex::Channel < ActionCable::Channel::Base
       reflex = reflex_class.new(self, url: url, element: element, selectors: selectors)
       delegate_call_to_reflex reflex, method_name, arguments
     rescue => invoke_error
-      message = invoke_error.backtrace.first
+      message = exception_message_with_backtrace(invoke_error)
       return broadcast_error("StimulusReflex::Channel Failed to invoke #{target}! #{url} #{message}", data)
     end
 
     begin
       render_page_and_broadcast_morph url, reflex, selectors, data
     rescue => render_error
-      message = render_error.backtrace.first
+      message = exception_message_with_backtrace(render_error)
       broadcast_error "StimulusReflex::Channel Failed to re-render #{url} #{message}", data
     end
   end
@@ -122,5 +122,9 @@ class StimulusReflex::Channel < ActionCable::Channel::Base
       detail: {stimulus_reflex: data.merge(error: message)}
     )
     cable_ready.broadcast
+  end
+
+  def exception_message_with_backtrace(exception)
+    "#{exception} #{exception.backtrace.first}"
   end
 end
