028e177d5217ecfe7a6a250afe46c493a9d9cf7e
Hopsoft <natehop@gmail.com>
2020-03-27 12:45:46 -0600

    Reload session prior to each reflex accessing it

--- lib/stimulus_reflex/reflex.rb
+++ lib/stimulus_reflex/reflex.rb
@@ -14,6 +14,31 @@ class StimulusReflex::Reflex
   end
 
   def request
-    @request ||= ActionDispatch::Request.new(connection.env)
+    @request ||= begin
+      uri = URI.parse(url)
+      path = ActionDispatch::Journey::Router::Utils.normalize_path(uri.path)
+      query_hash = Rack::Utils.parse_nested_query(uri.query)
+      request = ActionDispatch::Request.new(
+        connection.env.merge(
+          Rack::MockRequest.env_for(uri.to_s).merge(
+            "rack.request.query_hash" => query_hash,
+            "rack.request.query_string" => uri.query,
+            "ORIGINAL_SCRIPT_NAME" => "",
+            "ORIGINAL_FULLPATH" => path,
+            Rack::SCRIPT_NAME => "",
+            Rack::PATH_INFO => path,
+            Rack::REQUEST_PATH => path,
+            Rack::QUERY_STRING => uri.query,
+            ActionDispatch::Http::Parameters::PARAMETERS_KEY => url_params.symbolize_keys
+          )
+        )
+      )
+      request.session.send :load!
+      request
+    end
+  end
+
+  def url_params
+    @url_params ||= Rails.application.routes.recognize_path_with_request(request, request.path, request.env[:extras] || {})
   end
 end
