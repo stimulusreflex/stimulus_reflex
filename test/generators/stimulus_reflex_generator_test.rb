26652c005321e6f54e7ed32984bd1ecc04dedcfc
darkrubyist <chirvase.dragos.andrei@gmail.com>
2020-04-29 19:35:52 +0300

    Add passing tests for both singular and plural scenarios

--- test/generators/stimulus_reflex_generator_test.rb
+++ test/generators/stimulus_reflex_generator_test.rb
@@ -8,11 +8,19 @@ class StimulusReflexGeneratorTest < Rails::Generators::TestCase
   destination File.expand_path("../../tmp", __FILE__)
   setup :prepare_destination
 
-  test "creates named controller and reflex files" do
+  test "creates singular named controller and reflex files" do
     run_generator %w[demo]
     assert_file "app/javascript/controllers/application_controller.js"
     assert_file "app/javascript/controllers/demo_controller.js", /DemoReflex/
     assert_file "app/reflexes/application_reflex.rb"
-    assert_file "app/reflexes/demo_reflex.rb", /DemosReflex/
+    assert_file "app/reflexes/demo_reflex.rb", /DemoReflex/
+  end
+
+  test "creates plural named controller and reflex files" do
+    run_generator %w[posts]
+    assert_file "app/javascript/controllers/application_controller.js"
+    assert_file "app/javascript/controllers/posts_controller.js", /PostsReflex/
+    assert_file "app/reflexes/application_reflex.rb"
+    assert_file "app/reflexes/posts_reflex.rb", /PostsReflex/
   end
 end
