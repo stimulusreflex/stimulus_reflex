a91a7223ff69518003b2ea85c920ad655e636bd1
Sebastien <sebastienauriault@gmail.com>
2020-05-05 23:07:26 -0400

    add reflex callbacks (#160)
    * Add reflex callbacks
    * Add callback around_reflex, optional blocks, new options and rename action to reflex
    * Some light refactoring and added support for halting the reflex
    
    Co-authored-by: Hopsoft <natehop@gmail.com>

--- lib/stimulus_reflex/reflex.rb
+++ lib/stimulus_reflex/reflex.rb
@@ -1,16 +1,57 @@
 # frozen_string_literal: true
 
 class StimulusReflex::Reflex
-  attr_reader :channel, :url, :element, :selectors
+  include ActiveSupport::Callbacks
+
+  define_callbacks :process, skip_after_callbacks_if_terminated: true
+
+  class << self
+    def before_reflex(*args, &block)
+      add_callback(:before, *args, &block)
+    end
+
+    def after_reflex(*args, &block)
+      add_callback(:after, *args, &block)
+    end
+
+    def around_reflex(*args, &block)
+      add_callback(:around, *args, &block)
+    end
+
+    private
+
+    def add_callback(kind, *args, &block)
+      options = args.extract_options!
+      options.assert_valid_keys :if, :unless, :only, :except
+      set_callback(*[:process, kind, args, normalize_callback_options!(options)].flatten, &block)
+    end
+
+    def normalize_callback_options!(options)
+      normalize_callback_option! options, :only, :if
+      normalize_callback_option! options, :except, :unless
+      options
+    end
+
+    def normalize_callback_option!(options, from, to)
+      if (from = options.delete(from))
+        from_set = Array(from).map(&:to_s).to_set
+        from = proc { |reflex| from_set.include? reflex.reflex_name }
+        options[to] = Array(options[to]).unshift(from)
+      end
+    end
+  end
+
+  attr_reader :channel, :url, :element, :selectors, :reflex_name
 
   delegate :connection, to: :channel
   delegate :session, to: :request
 
-  def initialize(channel, url: nil, element: nil, selectors: [])
+  def initialize(channel, url: nil, element: nil, selectors: [], reflex_name: nil)
     @channel = channel
     @url = url
     @element = element
     @selectors = selectors
+    @reflex_name = reflex_name
   end
 
   def request
@@ -40,5 +81,18 @@ class StimulusReflex::Reflex
 
   def url_params
     @url_params ||= Rails.application.routes.recognize_path_with_request(request, request.path, request.env[:extras] || {})
+  end
+
+  def process(name, *args)
+    result = run_callbacks(:process) { public_send(name, *args) }
+    @halted ||= result == false
+    result
+  end
+
+  # Indicates if the callback chain was halted via a throw(:abort) in a before_reflex callback.
+  # SEE: https://api.rubyonrails.org/classes/ActiveSupport/Callbacks.html
+  # IMPORTANT: The reflex will not re-render the page if the callback chain is halted
+  def halted?
+    !!@halted
   end
 end
