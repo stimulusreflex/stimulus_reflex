7539ab03df8b0dca149e1b97c8bdf09b647cdc19
Marco Roth <marco.roth@intergga.ch>
2020-05-23 18:58:32 +0200

    add lost ['attrs'] back from merge

--- lib/stimulus_reflex/element.rb
+++ lib/stimulus_reflex/element.rb
@@ -4,8 +4,8 @@ class StimulusReflex::Element < OpenStruct
   attr_reader :attributes, :data_attributes
 
   def initialize(attrs = {})
-    @attributes = HashWithIndifferentAccess.new(attrs || {})
-    @data_attributes = (attrs["dataset"] || {}).each_with_object(HashWithIndifferentAccess.new) { |(key, value), memo|
+    @attributes = HashWithIndifferentAccess.new(attrs.to_h["attrs"] || {})
+    @data_attributes = (attrs.to_h["dataset"] || {}).each_with_object(HashWithIndifferentAccess.new) { |(key, value), memo|
       memo[key.delete_prefix("data-")] = value
     }.freeze
 
