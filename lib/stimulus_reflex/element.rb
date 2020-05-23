2d9298579b2d946030270a55ec14b72af566c8fb
Hopsoft <natehop@gmail.com>
2020-05-22 22:39:07 -0600

    Freeze the structs

--- lib/stimulus_reflex/element.rb
+++ lib/stimulus_reflex/element.rb
@@ -6,8 +6,10 @@ class StimulusReflex::Element < OpenStruct
   def initialize(attrs = {})
     @attributes = HashWithIndifferentAccess.new(attrs || {})
     data_attributes = @attributes.select { |key, _| key.start_with? "data-" }
-    data_attributes.transform_keys! { |key| key.delete_prefix("data-").underscore }
-    @dataset = OpenStruct.new(data_attributes.freeze)
-    super @attributes.merge(@attributes.transform_keys(&:underscore)).freeze
+    data_attributes.transform_keys! { |key| key.delete_prefix("data-") }
+    @dataset = OpenStruct.new(data_attributes.merge(data_attributes.transform_keys(&:underscore)))
+    @dataset.freeze
+    super @attributes.merge(@attributes.transform_keys(&:underscore))
+    freeze
   end
 end
