b3855190f61c3b2c3344643cfd4c3ffddfd35633
Hopsoft <natehop@gmail.com>
2020-05-22 23:12:26 -0600

    Restructure and cleanup

--- lib/stimulus_reflex/element.rb
+++ lib/stimulus_reflex/element.rb
@@ -1,15 +1,17 @@
 # frozen_string_literal: true
 
 class StimulusReflex::Element < OpenStruct
-  attr_reader :attributes, :dataset
+  attr_reader :attributes, :data_attributes
 
   def initialize(attrs = {})
     @attributes = HashWithIndifferentAccess.new(attrs || {})
-    data_attributes = @attributes.select { |key, _| key.start_with? "data-" }
-    data_attributes.transform_keys! { |key| key.delete_prefix("data-") }
-    @dataset = OpenStruct.new(data_attributes.merge(data_attributes.transform_keys(&:underscore)))
-    @dataset.freeze
-    super @attributes.merge(@attributes.transform_keys(&:underscore))
-    freeze
+    @data_attributes = attributes.each_with_object(HashWithIndifferentAccess.new) { |(key, value), memo|
+      memo[key.delete_prefix("data-")] = value if key.start_with?("data-")
+    }
+    super attributes.merge(attributes.transform_keys(&:underscore))
+  end
+
+  def dataset
+    @dataset ||= OpenStruct.new(data_attributes.merge(data_attributes.transform_keys(&:underscore)))
   end
 end
