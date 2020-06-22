5f6dc108ee0f0d70950bd0ea28817718b07b2ab7
paramagicdev <konnor5456@gmail.com>
2020-06-22 10:02:58 -0400

    fix things fixed by rubocop

--- lib/stimulus_reflex.rb
+++ lib/stimulus_reflex.rb
@@ -1,19 +1,18 @@
 # frozen_string_literal: true
 
-require 'uri'
-require 'rack'
-require 'rails/engine'
-require 'active_support/all'
-require 'action_dispatch'
-require 'action_cable'
-require 'nokogiri'
-require 'cable_ready'
-require 'stimulus_reflex/version'
-require 'stimulus_reflex/reflex'
-require 'stimulus_reflex/element'
-require 'stimulus_reflex/rails/actioncable/channel/base'
-require 'stimulus_reflex/channel'
-require 'generators/stimulus_reflex_generator'
+require "uri"
+require "rack"
+require "rails/engine"
+require "active_support/all"
+require "action_dispatch"
+require "action_cable"
+require "nokogiri"
+require "cable_ready"
+require "stimulus_reflex/version"
+require "stimulus_reflex/reflex"
+require "stimulus_reflex/element"
+require "stimulus_reflex/channel"
+require "generators/stimulus_reflex_generator"
 
 module StimulusReflex
   class Engine < Rails::Engine
