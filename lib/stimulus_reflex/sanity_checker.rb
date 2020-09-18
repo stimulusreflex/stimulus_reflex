90dc368ae139f4cb0de5fb3c304320dfebfb4530
Chris Oliver <excid3@gmail.com>
2020-09-18 12:04:16 -0500

    Refactor sanity checker

--- lib/stimulus_reflex/sanity_checker.rb
+++ lib/stimulus_reflex/sanity_checker.rb
@@ -6,10 +6,9 @@ module SanityChecker
     def check_caching_enabled
       unless caching_enabled?
         puts <<~WARN
-          Stimulus Reflex requires caching to be enabled. Caching allows the session to be modified during ActionCable requests.
+          WARNING: Stimulus Reflex requires caching to be enabled. Caching allows the session to be modified during ActionCable requests.
           To enable caching in development, run:
-
-            rails dev:cache
+              rails dev:cache
         WARN
       end
     end
@@ -17,17 +16,16 @@ module SanityChecker
     def check_javascript_package_version
       if javascript_package_version.nil?
         puts <<~WARN
-          Can't locate the stimulus_reflex NPM package.
+          WARNING: Can't locate the stimulus_reflex NPM package.
           Either add it to your package.json as a dependency or use "yarn link stimulus_reflex" if you are doing development.
         WARN
       end
 
       unless javascript_version_matches?
         puts <<~WARN
-          The Stimulus Reflex javascript package version (#{javascript_package_version}) does not match the Rubygem version (#{gem_version}).
+          WARNING: The Stimulus Reflex javascript package version (#{javascript_package_version}) does not match the Rubygem version (#{gem_version}).
           To update the Stimulus Reflex npm package:
-
-            yarn upgrade stimulus_reflex@#{gem_version}
+              yarn upgrade stimulus_reflex@#{gem_version}
         WARN
       end
     end
@@ -44,19 +42,25 @@ module SanityChecker
     end
 
     def gem_version
-      StimulusReflex::VERSION.gsub(".pre", "-pre")
+      @_gem_version ||= StimulusReflex::VERSION.gsub(".pre", "-pre")
     end
 
     def javascript_package_version
-      if File.exist?(yarn_lock_path)
-        match = File.foreach(yarn_lock_path).grep(/^stimulus_reflex/)
-        return match.first[NODE_VERSION_FORMAT, 1] if match.present?
-      end
+      return @_js_version if defined?(@_js_version)
+      @_js_version = find_javascript_package_version
+    end
 
-      if File.exist?(yarn_link_path)
-        match = File.foreach(yarn_link_path).grep(/version/)
-        return match.first[JSON_VERSION_FORMAT, 1] if match.present?
+    def find_javascript_package_version
+      if (match = search_file(yarn_lock_path, regex: /^stimulus_reflex/))
+        match[NODE_VERSION_FORMAT, 1]
+      elsif (match = search_file(yarn_link_path, regex: /version/))
+        match[JSON_VERSION_FORMAT, 1]
       end
+    end
+
+    def search_file(path, regex:)
+      return unless File.exist?(path)
+      File.foreach(path).grep(regex).first
     end
 
     def yarn_lock_path
