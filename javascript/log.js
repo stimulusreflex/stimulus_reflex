a56a564d82602ed82697015091b9ec662e1d75e6
leastbad <hello@leastbad.com>
2020-09-28 01:34:10 -0400

    removed wireframe concept from PR; continued to iterate on log functions

--- javascript/log.js
+++ javascript/log.js
@@ -1,17 +1,11 @@
 const logs = {}
 
-function request (
-  reflexId,
-  target,
-  args,
-  stimulusControllerIdentifier,
-  element
-) {
+function request (reflexId, target, args, controller, element) {
   logs[reflexId] = new Date()
   console.log(`\u2191 stimulus \u2191 ${target}`, {
     reflexId,
     args,
-    stimulusControllerIdentifier,
+    controller,
     element
   })
 }
@@ -20,31 +14,42 @@ function success (event, options = { halted: false }) {
   const { detail } = event || {}
   const { selector } = detail || {}
   const { reflexId, target, morph } = event.detail.stimulusReflex || {}
-  const progress = options.completed
-    ? ` ${options.completed}/${options.total}`
-    : ''
+  const progress =
+    options.completed && options.total > 1
+      ? ` ${options.completed}/${options.total}`
+      : ''
+  const duration = `${new Date() - logs[reflexId]}ms`
+  const operation = event.type
+    .split(':')[1]
+    .split('-')
+    .slice(1)
+    .join('_')
 
-  console.log(`\u2193 reflex \u2193 ${target}${progress}`, {
-    reflexId,
-    duration: `${new Date() - logs[reflexId]}ms`,
-    halted: options.halted,
-    morph,
-    selector
-  })
+  console.log(
+    `\u2193 reflex \u2193 ${target} \u2192 ${selector}${progress} in ${duration}`,
+    {
+      reflexId,
+      morph,
+      operation,
+      halted: options.halted
+    }
+  )
 }
 
 function error (event) {
   const { detail } = event || {}
   const { selector } = detail || {}
   const { reflexId, target, error, morph } = detail.stimulusReflex || {}
-  console.error(`\u2193 reflex \u2193 ${target}`, {
-    reflexId,
-    duration: `${new Date() - logs[reflexId]}ms`,
-    error,
-    morph,
-    payload: event.detail.stimulusReflex,
-    selector
-  })
+  const duration = `${new Date() - logs[reflexId]}ms`
+  console.error(
+    `\u2193 reflex \u2193 ${target} \u2192 ${selector} in ${duration}`,
+    {
+      reflexId,
+      error,
+      morph,
+      payload: event.detail.stimulusReflex
+    }
+  )
   if (detail.stimulusReflex.serverMessage.body)
     console.error(
       `\u2193 reflex \u2193 ${target}`,
