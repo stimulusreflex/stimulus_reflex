5aaae94bdd99df2a6111fec3986de8e625bb244c
Piotr <piotr.wodz@yahoo.com>
2020-10-25 21:00:47 +0000

    Change config template identation

--- lib/generators/templates/stimulus_reflex_config.rb
+++ lib/generators/templates/stimulus_reflex_config.rb
@@ -1,4 +1,4 @@
 StimulusReflex.configure do |config|
-	# config.debugging = true
-	# config.logging = %i[session_id reflex_name operation connection_id timestamp]
+  # config.debugging = true
+  # config.logging = %i[session_id reflex_name operation connection_id timestamp]
 end
