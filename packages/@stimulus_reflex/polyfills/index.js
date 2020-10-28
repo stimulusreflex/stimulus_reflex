3c627f14b92b7f801a19889a13f14a1ac6916fba
Marco Roth <marco.roth@intergga.ch>
2020-10-28 09:13:45 +0100

    add string/starts-with polyfill

--- packages/@stimulus_reflex/polyfills/index.js
+++ packages/@stimulus_reflex/polyfills/index.js
@@ -2,6 +2,7 @@ import "@stimulus/polyfills"
 
 import "core-js/es/array/for-each"
 import "core-js/es/array/includes"
+import "core-js/es/string/starts-with"
 
 import "./polyfills/node-list/for-each"
 import "./polyfills/custom-event/custom-event"
