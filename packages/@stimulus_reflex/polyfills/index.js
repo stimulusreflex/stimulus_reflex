eacd4127ba0fa6738408c608f54b5537a101f6c0
Marco Roth <marco.roth@intergga.ch>
2020-10-29 16:36:44 +0100

    add string/virtual/includes to polyfills package

--- packages/@stimulus_reflex/polyfills/index.js
+++ packages/@stimulus_reflex/polyfills/index.js
@@ -2,7 +2,9 @@ import '@stimulus/polyfills'
 
 import 'core-js/es/array/for-each'
 import 'core-js/es/array/includes'
+
 import 'core-js/es/string/starts-with'
+import 'core-js/es/string/virtual/includes'
 
 import './polyfills/node-list/for-each'
 import './polyfills/custom-event/custom-event'
