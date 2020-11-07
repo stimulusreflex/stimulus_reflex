48f6f52f02602920e4245b94c7af8fe088a0c650
Piotr <piotr.wodz@yahoo.com>
2020-11-07 00:06:01 +0000

    Refactor stimulus reflex logger class

--- lib/stimulus_reflex/logger.rb
+++ lib/stimulus_reflex/logger.rb
@@ -5,14 +5,14 @@ module StimulusReflex
     attr_accessor :reflex
 
     COLORS = {
-      "black" => "0;30",
-      "red" => "0;31",
-      "green" => "0;32",
-      "yellow" => "0;33",
-      "blue" => "0;34",
-      "magenta" => "0;35",
-      "cyan" => "0;36",
-      "white" => "0;37"
+      "black" => "30",
+      "red" => "31",
+      "green" => "32",
+      "yellow" => "33",
+      "blue" => "34",
+      "magenta" => "35",
+      "cyan" => "36",
+      "white" => "37"
     }
 
     def initialize(reflex)
@@ -20,11 +20,9 @@ module StimulusReflex
     end
 
     def print
-      return if debugging? || log.empty?
+			return if debugging? || log.empty?
 
-      puts <<~WARN
-        #{self.class} logging #{colorized_log_message}
-      WARN
+      Kernel.print("#{self.class} logging #{colorized_log_message}")
     end
 
     private
@@ -33,17 +31,17 @@ module StimulusReflex
       StimulusReflex.config.debug
     end
 
-    def colorized_log_message
-      log.map { |element| colorize(element) }.push("\e[0;0m").join(" ")
+		def colorized_log_message
+      log.map { |element| colorize(element) }.push("\e[0m").join
     end
 
     def colorize(element)
       return element unless COLORS.key?(element)
 
       "\e[#{COLORS[element]}m"
-    end
+		end
 
-    def log
+		def log
       @log ||= StimulusReflex.config.logging.map { |element| element.is_a?(Symbol) ? send(element) : element }
     end
 
@@ -59,20 +57,16 @@ module StimulusReflex
       dataset.reflex.split("->").last
     end
 
-    def operation
-      "#{broadcaster}: #{selectors}"
-    end
-
     def broadcaster
       reflex.broadcaster.to_sym
     end
 
-    def selectors
+    def selector
       reflex.selectors.last
     end
 
-    def connection_id(identifier = reflex.connection&.connection_identifier)
-      return "-" if identifier.nil?
+		def connection_id(identifier = reflex.connection&.connection_identifier)
+      return "-" if identifier.empty?
 
       identifier
     end
