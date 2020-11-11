b64767c2eb7294060c21b8e8309c5b3f7529bfba
leastbad <38150464+leastbad@users.noreply.github.com>
2020-11-11 13:08:17 -0500

    Update stimulus_reflex.rb

--- lib/generators/stimulus_reflex/templates/config/initializers/stimulus_reflex.rb
+++ lib/generators/stimulus_reflex/templates/config/initializers/stimulus_reflex.rb
@@ -3,7 +3,7 @@
 StimulusReflex.configure do |config|
   # Enable/disable exiting / warning when the sanity checks fail options:
   # `:exit` or `:warn` or `:ignore`
-  # config.on_failed_sanity_checks = true
+  # config.on_failed_sanity_checks = :exit
 
   # Override the parent class that the StimulusReflex ActionCable channel inherits from
   # config.parent_channel = "ApplicationCable::Channel"
