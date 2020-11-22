a79dfad5d92998f0e3395f501601118db9a2d8df
Rafe Rosen <rafe@hey.com>
2020-11-22 13:42:11 -0500

    run standardize

--- javascript/stimulus_reflex.js
+++ javascript/stimulus_reflex.js
@@ -83,8 +83,11 @@ const createSubscription = controller => {
         if (!reflexes[reflexId] && !isolationMode) {
           const element = xPathToElement(reflexData.xpath)
           const controllerElement = xPathToElement(reflexData.cXpath)
-          if (element.reflexController == undefined) element.reflexController = {}
-          element.reflexController[reflexId] = stimulusApplication.getControllerForElementAndIdentifier(
+          if (element.reflexController == undefined)
+            element.reflexController = {}
+          element.reflexController[
+            reflexId
+          ] = stimulusApplication.getControllerForElementAndIdentifier(
             controllerElement,
             reflexData.reflexController
           )
@@ -205,7 +208,7 @@ const extendStimulusController = controller => {
         throw 'The ActionCable channel subscription for StimulusReflex was rejected.'
 
       // lifecycle setup
-      if (element.reflexController == undefined) element.reflexController = {}    
+      if (element.reflexController == undefined) element.reflexController = {}
       element.reflexController[reflexId] = this
       if (element.reflexData == undefined) element.reflexData = {}
       element.reflexData[reflexId] = data
