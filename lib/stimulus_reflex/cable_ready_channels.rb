449656c7ab1d97ac83e7dc55d1c35c07d2202b50
Hopsoft <natehop@gmail.com>
2020-11-26 08:25:46 -0700

    Setup a proxy object that wraps CableReady::Channels
    This improves developer ergonimics for emitting CR broadcasts on the SR
    channel within a reflex.

---
+++ lib/stimulus_reflex/cable_ready_channels.rb
@@ -0,0 +1,18 @@
+module StimulusReflex
+  class CableReadyChannels
+    def initialize(stream_name)
+      @cable_ready_channels = CableReady::Channels.instance
+      @stimulus_reflex_channel = @cable_ready_channels[stream_name]
+    end
+
+    def method_missing(name, *args)
+      return @cable_ready_channels.send(name, *args) if @cable_ready_channels.respond_to?(name)
+      @stimulus_reflex_channel.send(name, *args)
+    end
+
+    def respond_to_missing?(name, include_all)
+      @cable_ready_channels.respond_to?(name, include_all) ||
+        @stimulus_reflex_channel.respond_to?(name, include_all)
+    end
+  end
+end
