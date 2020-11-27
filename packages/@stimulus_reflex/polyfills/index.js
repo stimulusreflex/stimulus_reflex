92035522cf27a63167eece025fde2d8e3a006d1d
Marco Roth <marco.roth@intergga.ch>
2020-11-27 17:08:50 +0100

    include polyfill for `Reflect.deleteProperty()`
    introduced in #385

--- packages/@stimulus_reflex/polyfills/index.js
+++ packages/@stimulus_reflex/polyfills/index.js
@@ -4,6 +4,8 @@ import '@cable_ready/polyfills'
 import 'core-js/es/string/starts-with'
 import 'core-js/es/string/virtual/includes'
 
+import 'core-js/es/reflect/delete-property'
+
 import './polyfills/node-list/for-each'
 
 import 'formdata-polyfill'
