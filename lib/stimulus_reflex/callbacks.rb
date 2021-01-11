97965ec10d4a3d9bb28b930929de81fd17bac73f
Julian Rubisch <julian@julianrubisch.at>
2021-01-11 12:09:46 +0100

    Use ActiveSupport::Concern for syntactic sugar

--- lib/stimulus_reflex/callbacks.rb
+++ lib/stimulus_reflex/callbacks.rb
@@ -1,44 +1,46 @@
+require "active_support/concern"
+
 module StimulusReflex
   module Callbacks
-    def self.included(klass)
-      klass.class_eval do
-        include ActiveSupport::Callbacks
-        define_callbacks :process, skip_after_callbacks_if_terminated: true
+    extend ActiveSupport::Concern
 
-        class << self
-          def before_reflex(*args, &block)
-            add_callback(:before, *args, &block)
-          end
+    included do
+      include ActiveSupport::Callbacks
+      define_callbacks :process, skip_after_callbacks_if_terminated: true
+    end
 
-          def after_reflex(*args, &block)
-            add_callback(:after, *args, &block)
-          end
+    class_methods do
+      def before_reflex(*args, &block)
+        add_callback(:before, *args, &block)
+      end
 
-          def around_reflex(*args, &block)
-            add_callback(:around, *args, &block)
-          end
+      def after_reflex(*args, &block)
+        add_callback(:after, *args, &block)
+      end
 
-          private
+      def around_reflex(*args, &block)
+        add_callback(:around, *args, &block)
+      end
 
-          def add_callback(kind, *args, &block)
-            options = args.extract_options!
-            options.assert_valid_keys :if, :unless, :only, :except
-            set_callback(*[:process, kind, args, normalize_callback_options!(options)].flatten, &block)
-          end
+      private
 
-          def normalize_callback_options!(options)
-            normalize_callback_option! options, :only, :if
-            normalize_callback_option! options, :except, :unless
-            options
-          end
+      def add_callback(kind, *args, &block)
+        options = args.extract_options!
+        options.assert_valid_keys :if, :unless, :only, :except
+        set_callback(*[:process, kind, args, normalize_callback_options!(options)].flatten, &block)
+      end
 
-          def normalize_callback_option!(options, from, to)
-            if (from = options.delete(from))
-              from_set = Array(from).map(&:to_s).to_set
-              from = proc { |reflex| from_set.include? reflex.method_name }
-              options[to] = Array(options[to]).unshift(from)
-            end
-          end
+      def normalize_callback_options!(options)
+        normalize_callback_option! options, :only, :if
+        normalize_callback_option! options, :except, :unless
+        options
+      end
+
+      def normalize_callback_option!(options, from, to)
+        if (from = options.delete(from))
+          from_set = Array(from).map(&:to_s).to_set
+          from = proc { |reflex| from_set.include? reflex.method_name }
+          options[to] = Array(options[to]).unshift(from)
         end
       end
     end
