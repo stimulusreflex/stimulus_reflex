69c0c35cf9be56a5964767d45b86d6a9cda57142
Leandro Matioli <matioli.leandro@gmail.com>
2021-04-23 14:50:20 -0300

    Uncomment selector_broadcaster_test to improve our coverage.
    The tests are passing and it will help us to continue adding more
    scenarios to check (like the whitespace issue seen on #487).

--- test/broadcasters/selector_broadcaster_test.rb
+++ test/broadcasters/selector_broadcaster_test.rb
@@ -1,59 +1,61 @@
 # frozen_string_literal: true
 
-# require_relative "broadcaster_test_case"
+require_relative "broadcaster_test_case"
 
-# class StimulusReflex::SelectorBroadcasterTest < StimulusReflex::BroadcasterTestCase
-# test "morphs the contents of an element if the selector(s) are present in both original and morphed html fragments" do
-# broadcaster = StimulusReflex::SelectorBroadcaster.new(@reflex)
-# broadcaster.append_morph("#foo", "<div id=\"foo\"><span>bar</span></div>")
+module StimulusReflex
+  class SelectorBroadcasterTest < StimulusReflex::BroadcasterTestCase
+    test "morphs the contents of an element if the selector(s) are present in both original and morphed html fragments" do
+      broadcaster = StimulusReflex::SelectorBroadcaster.new(@reflex)
+      broadcaster.append_morph("#foo", '<div id="foo"><span>bar</span></div>')
 
-# expected = {
-# "cableReady" => true,
-# "operations" => {
-# "morph" => [
-# {
-# "selector" => "#foo",
-# "html" => "<span>bar</span>",
-# "childrenOnly" => true,
-# "permanentAttributeName" => nil,
-# "stimulusReflex" => {
-# "payload" => {},
-# "some" => :data,
-# "morph" => :selector
-# }
-# }
-# ]
-# }
-# }
+      expected = {
+        "cableReady" => true,
+        "operations" => {
+          "morph" => [
+            {
+              "selector" => "#foo",
+              "html" => "<span>bar</span>",
+              "payload" => {},
+              "childrenOnly" => true,
+              "permanentAttributeName" => nil,
+              "stimulusReflex" => {
+                "some" => :data,
+                "morph" => :selector
+              }
+            }
+          ]
+        }
+      }
 
-# assert_broadcast_on @reflex.stream_name, expected do
-# broadcaster.broadcast nil, some: :data
-# end
-# end
+      assert_broadcast_on @reflex.stream_name, expected do
+        broadcaster.broadcast nil, some: :data
+      end
+    end
 
-# test "replaces the contents of an element and ignores permanent-attributes if the selector(s) aren't present in the replacing html fragment" do
-# broadcaster = StimulusReflex::SelectorBroadcaster.new(@reflex)
-# broadcaster.append_morph("#foo", "<div id=\"baz\"><span>bar</span></div>")
+    test "replaces the contents of an element and ignores permanent-attributes if the selector(s) aren't present in the replacing html fragment" do
+      broadcaster = StimulusReflex::SelectorBroadcaster.new(@reflex)
+      broadcaster.append_morph("#foo", '<div id="baz"><span>bar</span></div>')
 
-# expected = {
-# "cableReady" => true,
-# "operations" => {
-# "innerHtml" => [
-# {
-# "selector" => "#foo",
-# "html" => "<div id=\"baz\"><span>bar</span></div>",
-# "stimulusReflex" => {
-# "payload" => {},
-# "some" => :data,
-# "morph" => :selector
-# }
-# }
-# ]
-# }
-# }
+      expected = {
+        "cableReady" => true,
+        "operations" => {
+          "innerHtml" => [
+            {
+              "selector" => "#foo",
+              "html" => '<div id="baz"><span>bar</span></div>',
+              "payload" => {},
+              "stimulusReflex" => {
+                "some" => :data,
+                "morph" => :selector
+              }
+            }
+          ]
+        }
+      }
 
-# assert_broadcast_on @reflex.stream_name, expected do
-# broadcaster.broadcast nil, some: :data
-# end
-# end
-# end
+      assert_broadcast_on @reflex.stream_name, expected do
+        broadcaster.broadcast nil, some: :data
+      end
+    end
+  end
+end
