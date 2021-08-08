3de919dab777f496d2a558d0f27ccdbf54befcd2
leastbad <hello@leastbad.com>
2021-08-08 01:26:12 -0400

    updated tests

--- test/broadcasters/nothing_broadcaster_test.rb
+++ test/broadcasters/nothing_broadcaster_test.rb
@@ -3,35 +3,27 @@
 require_relative "broadcaster_test_case"
 
 class StimulusReflex::NothingBroadcasterTest < StimulusReflex::BroadcasterTestCase
-  test "broadcasts a server message when called" do
+  test "broadcasts a nothing morph when called" do
     broadcaster = StimulusReflex::NothingBroadcaster.new(@reflex)
 
     expected = {
       "cableReady" => true,
-      "operations" => {
-        "dispatchEvent" => [
-          {
-            "name" => "stimulus-reflex:server-message",
-            "detail" => {
-              "reflexId" => "666",
-              "payload" => {},
-              "stimulusReflex" => {
-                "some" => :data,
-                "morph" => :nothing,
-                "serverMessage" => {
-                  "subject" => "nothing",
-                  "body" => nil
-                }
-              }
-            },
-            "reflexId" => "666"
-          }
-        ]
-      }
+      "operations" => [
+        {
+          "name" => "stimulus-reflex:morph-nothing",
+          "payload" => {},
+          "stimulusReflex" => {
+            "some" => "data",
+            "morph" => "nothing"
+          },
+          "reflexId" => "666",
+          "operation" => "dispatchEvent"
+        }
+      ]
     }
 
     assert_broadcast_on @reflex.stream_name, expected do
-      broadcaster.broadcast nil, {:some => :data, "reflexId" => "666"}
+      broadcaster.broadcast nil, {some: :data}
     end
   end
 end
