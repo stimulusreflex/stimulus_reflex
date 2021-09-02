7aa3a973bfcd35fd1744c1b90d1eb0583cfb21bc
Nachiket Pusalkar <33430835+nachiket87@users.noreply.github.com>
2021-09-02 02:31:54 -0400

    Add generator options to skip reflex and stimulus (#543)
    * feature: add generator options to skip reflex and stimulus
    
    * add additional options for generator
    
    * update tests
    
    * replace application with app in generator

--- lib/generators/stimulus_reflex/stimulus_reflex_generator.rb
+++ lib/generators/stimulus_reflex/stimulus_reflex_generator.rb
@@ -7,20 +7,21 @@ class StimulusReflexGenerator < Rails::Generators::NamedBase
 
   argument :name, type: :string, required: true, banner: "NAME"
   argument :actions, type: :array, default: [], banner: "action action"
+  class_options skip_stimulus: false, skip_app_reflex: false, skip_reflex: false, skip_app_controller: false
 
   def execute
     actions.map!(&:underscore)
 
     copy_application_files if behavior == :invoke
 
-    template "app/reflexes/%file_name%_reflex.rb"
-    template "app/javascript/controllers/%file_name%_controller.js"
+    template "app/reflexes/%file_name%_reflex.rb" unless options[:skip_reflex]
+    template "app/javascript/controllers/%file_name%_controller.js" unless options[:skip_stimulus]
   end
 
   private
 
   def copy_application_files
-    template "app/reflexes/application_reflex.rb"
-    template "app/javascript/controllers/application_controller.js"
+    template "app/reflexes/application_reflex.rb" unless options[:skip_app_reflex]
+    template "app/javascript/controllers/application_controller.js" unless options[:skip_app_controller]
   end
 end
