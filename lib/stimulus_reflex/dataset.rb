e9f7fa3702511d6e2a24be708eb99609811b7413
Julian Rubisch <julian@julianrubisch.at>
2021-09-05 10:42:36 +0200

    Move all of dataset initialization to dataset

--- lib/stimulus_reflex/dataset.rb
+++ lib/stimulus_reflex/dataset.rb
@@ -5,8 +5,15 @@ require "stimulus_reflex/utils/attribute_builder"
 class StimulusReflex::Dataset < OpenStruct
   include StimulusReflex::AttributeBuilder
 
-  def initialize(datasets)
-    super datasets
+  attr_accessor :attrs, :data_attrs
+
+  def initialize(data = {})
+    datasets = data["dataset"] || {}
+    regular_dataset = datasets["dataset"] || {}
+    @attrs = build_data_attrs(regular_dataset, datasets["datasetAll"] || {})
+    @data_attrs = @attrs.transform_keys { |key| key.delete_prefix "data-" }
+
+    super build_underscored(@data_attrs)
   end
 
   def signed
--- lib/stimulus_reflex/element.rb
+++ lib/stimulus_reflex/element.rb
@@ -6,22 +6,18 @@ require "stimulus_reflex/utils/attribute_builder"
 class StimulusReflex::Element < OpenStruct
   include StimulusReflex::AttributeBuilder
 
-  attr_reader :attrs, :data_attrs, :dataset
+  attr_reader :attrs, :dataset
 
   alias_method :data_attributes, :dataset
 
-  delegate :signed, :unsigned, :numeric, :truthy?, to: :dataset
+  delegate :signed, :unsigned, :numeric, :truthy?, :data_attrs, to: :dataset
 
   def initialize(data = {})
     @attrs = HashWithIndifferentAccess.new(data["attrs"] || {})
-    datasets = data["dataset"] || {}
-    regular_dataset = datasets["dataset"] || {}
-    @data_attrs = build_data_attrs(regular_dataset, datasets["datasetAll"] || {})
-    all_attributes = @attrs.merge(@data_attrs)
-    super build_underscored(all_attributes)
-    @data_attrs.transform_keys! { |key| key.delete_prefix "data-" }
+    @dataset = StimulusReflex::Dataset.new(data)
 
-    @dataset = StimulusReflex::Dataset.new(build_underscored(data_attrs))
+    all_attributes = @attrs.merge(@dataset.attrs)
+    super build_underscored(all_attributes)
   end
 
   def attributes
