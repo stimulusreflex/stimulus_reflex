e00ef945b36ca554d1f7f7a90d939dbfcb91c206
leastbad <38150464+leastbad@users.noreply.github.com>
2021-09-27 23:28:37 -0400

    overhaul Reflex logging (#546)

--- app/channels/stimulus_reflex/channel.rb
+++ app/channels/stimulus_reflex/channel.rb
@@ -24,15 +24,15 @@ class StimulusReflex::Channel < StimulusReflex.configuration.parent_channel.cons
 
         if reflex
           reflex.rescue_with_handler(exception)
-          puts error_message
+          reflex.logger&.error error_message
           reflex.error data: data, body: "#{exception} #{exception.backtrace.first.split(":in ")[0] if Rails.env.development?}"
         else
-          puts error_message
+          reflex.logger&.error error_message
 
           if body.to_s.include? "No route matches"
             initializer_path = Rails.root.join("config", "initializers", "stimulus_reflex.rb")
 
-            puts <<~NOTE
+            reflex.logger&.warn <<~NOTE
               \e[33mNOTE: StimulusReflex failed to locate a matching route and could not re-render the page.
 
               If your app uses Rack middleware to rewrite part of the request path, you must enable those middleware modules in StimulusReflex.
@@ -62,14 +62,14 @@ class StimulusReflex::Channel < StimulusReflex.configuration.parent_channel.cons
           reflex.rescue_with_handler(exception)
           error = exception_with_backtrace(exception)
           reflex.error data: data, body: "#{exception} #{exception.backtrace.first.split(":in ")[0] if Rails.env.development?}"
-          puts "\e[31mReflex failed to re-render: #{error[:message]} [#{reflex_data.url}]\e[0m\n#{error[:stack]}"
+          reflex.logger&.error "\e[31mReflex failed to re-render: #{error[:message]} [#{reflex_data.url}]\e[0m\n#{error[:stack]}"
         end
       end
     ensure
       if reflex
         commit_session(reflex)
         report_failed_basic_auth(reflex) if reflex.controller?
-        reflex.logger&.print
+        reflex.logger&.log_all_operations
       end
     end
   end
@@ -97,12 +97,12 @@ class StimulusReflex::Channel < StimulusReflex.configuration.parent_channel.cons
     store.commit_session reflex.request, reflex.controller.response
   rescue => exception
     error = exception_with_backtrace(exception)
-    puts "\e[31mFailed to commit session! #{error[:message]}\e[0m\n#{error[:backtrace]}"
+    reflex.logger&.error "\e[31mFailed to commit session! #{error[:message]}\e[0m\n#{error[:backtrace]}"
   end
 
   def report_failed_basic_auth(reflex)
     if reflex.controller.response.status == 401
-      puts "\e[31mReflex failed to process controller action \"#{reflex.controller.class}##{reflex.controller.action_name}\" due to HTTP basic auth. Consider adding \"unless: -> { @stimulus_reflex }\" to the before_action or method responible for authentication.\e[0m"
+      reflex.logger&.error "\e[31mReflex failed to process controller action \"#{reflex.controller.class}##{reflex.controller.action_name}\" due to HTTP basic auth. Consider adding \"unless: -> { @stimulus_reflex }\" to the before_action or method responible for authentication.\e[0m"
     end
   end
 
