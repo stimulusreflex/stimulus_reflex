8d453fde950fffa0d2f9df4026fc530306126ceb
Marco Roth <marco.roth@intergga.ch>
2022-03-13 16:29:39 +0100

    additionally bundle into `app/assets/javascripts` for the gem to be picked up

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,34 @@
 import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
 import json from '@rollup/plugin-json'
+import { terser } from 'rollup-plugin-terser'
 
-const basePlugins = [
-  resolve(),
-  commonjs(),
-  json()
-]
+const pretty = () => {
+  return terser({
+    mangle: false,
+    compress: false,
+    format: {
+      beautify: true,
+      indent_level: 2
+    }
+  })
+}
+
+const minify = () => {
+  return terser({
+    mangle: true,
+    compress: true
+  })
+}
 
 export default [
   {
-    external: ['morphdom'],
+    external: [
+      'morphdom',
+      '@hotwired/stimulus',
+      'cable_ready',
+      '@rails/actioncable'
+    ],
     input: 'javascript/index.js',
     output: [
       {
@@ -19,15 +37,46 @@ export default [
         format: 'umd',
         sourcemap: true,
         exports: 'named',
-        globals: { morphdom: 'morphdom' }
+        globals: {
+          morphdom: 'morphdom',
+          'cable_ready': 'CableReady'
+        },
+        plugins: [
+          pretty()
+        ]
       },
       {
         file: 'dist/stimulus_reflex.module.js',
         format: 'es',
-        sourcemap: true
-      }
+        sourcemap: true,
+        inlineDynamicImports: true,
+        plugins: [
+          pretty()
+        ]
+      },
+      {
+        file: 'app/assets/javascripts/stimulus_reflex.js',
+        format: 'es',
+        inlineDynamicImports: true,
+        plugins: [
+          pretty()
+        ]
+      },
+      {
+        file: 'app/assets/javascripts/stimulus_reflex.min.js',
+        format: 'es',
+        sourcemap: true,
+        inlineDynamicImports: true,
+        plugins: [
+          minify()
+        ]
+      },
     ],
-    plugins: basePlugins,
+    plugins: [
+      commonjs(),
+      resolve(),
+      json()
+    ],
     watch: {
       include: 'javascript/**'
     }
