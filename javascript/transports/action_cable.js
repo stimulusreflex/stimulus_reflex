9d5945f945dbf64cd8fcfc6f6cdeae83fddf924e
leastbad <38150464+leastbad@users.noreply.github.com>
2022-06-15 06:24:11 -0400

    connection status class (#589)

--- javascript/transports/action_cable.js
+++ javascript/transports/action_cable.js
@@ -1,10 +1,26 @@
 import { createConsumer } from '@rails/actioncable'
 import { received } from '../reflexes'
 import { emitEvent } from '../utils'
+import Deprecate from '../deprecate'
 
 let consumer
 let params
 let subscriptionActive
+
+const initialize = (consumerValue, paramsValue) => {
+  consumer = consumerValue
+  params = paramsValue
+  document.addEventListener('DOMContentLoaded', () => {
+    subscriptionActive = false
+    connectionStatusClass()
+    if (Deprecate.enabled && consumer)
+      console.warn(
+        "Deprecation warning: the next version of StimulusReflex will obtain a reference to consumer via the Stimulus application object.\nPlease add 'application.consumer = consumer' to your index.js after your Stimulus application has been established, and remove the consumer key from your StimulusReflex initialize() options object."
+      )
+  })
+  document.addEventListener('turbolinks:load', connectionStatusClass)
+  document.addEventListener('turbo:load', connectionStatusClass)
+}
 
 const createSubscription = controller => {
   consumer = consumer || controller.application.consumer || createConsumer()
@@ -24,20 +40,14 @@ const createSubscription = controller => {
 
 const connected = () => {
   subscriptionActive = true
-  document.body.classList.replace(
-    'stimulus-reflex-disconnected',
-    'stimulus-reflex-connected'
-  )
+  connectionStatusClass()
   emitEvent('stimulus-reflex:connected')
   emitEvent('stimulus-reflex:action-cable:connected')
 }
 
 const rejected = () => {
   subscriptionActive = false
-  document.body.classList.replace(
-    'stimulus-reflex-connected',
-    'stimulus-reflex-disconnected'
-  )
+  connectionStatusClass()
   emitEvent('stimulus-reflex:rejected')
   emitEvent('stimulus-reflex:action-cable:rejected')
   if (Debug.enabled) console.warn('Channel subscription was rejected.')
@@ -45,12 +55,31 @@ const rejected = () => {
 
 const disconnected = willAttemptReconnect => {
   subscriptionActive = false
-  document.body.classList.replace(
-    'stimulus-reflex-connected',
-    'stimulus-reflex-disconnected'
-  )
+  connectionStatusClass()
   emitEvent('stimulus-reflex:disconnected', willAttemptReconnect)
   emitEvent('stimulus-reflex:action-cable:disconnected', willAttemptReconnect)
+}
+
+const connectionStatusClass = () => {
+  const list = document.body.classList
+  if (
+    !(
+      list.contains('stimulus-reflex-connected') ||
+      list.contains('stimulus-reflex-disconnected')
+    )
+  ) {
+    list.add(
+      subscriptionActive
+        ? 'stimulus-reflex-connected'
+        : 'stimulus-reflex-disconnected'
+    )
+    return
+  }
+  if (subscriptionActive) {
+    list.replace('stimulus-reflex-disconnected', 'stimulus-reflex-connected')
+  } else {
+    list.replace('stimulus-reflex-connected', 'stimulus-reflex-disconnected')
+  }
 }
 
 export default {
@@ -63,8 +92,6 @@ export default {
   connected,
   rejected,
   disconnected,
-  set (consumerValue, paramsValue) {
-    consumer = consumerValue
-    params = paramsValue
-  }
+  connectionStatusClass,
+  initialize
 }
