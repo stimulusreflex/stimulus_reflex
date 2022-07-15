c98eb5ee3954165afcb7a27bb018619492e040a9
Julian Rubisch <julian@julianrubisch.at>
2022-07-15 18:51:53 +0200

    chore: Extract StimulusReflex::CableReadyable module

---
+++ lib/stimulus_reflex/cable_readyable.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+require "active_support/concern"
+
+module StimulusReflex
+  module CableReadyable
+    extend ActiveSupport::Concern
+
+    prepended do
+      attr_reader :cable_ready
+    end
+
+    def initialize(*args, **kwargs)
+      super(*args, **kwargs)
+
+      if is_a? CableReady::Broadcaster
+        message = <<~MSG
+
+          #{self.class.name} includes CableReady::Broadcaster, and you need to remove it.
+          Reflexes have their own CableReady interface. You can just assume that it's present.
+          See https://docs.stimulusreflex.com/rtfm/cableready#using-cableready-inside-a-reflex-action for more details.
+
+        MSG
+        raise TypeError.new(message.strip)
+      end
+
+      @cable_ready = StimulusReflex::CableReadyChannels.new(stream_name, reflex_id)
+    end
+  end
+end
