8a043a63d22ad20fe592a0d4632bb3c2943c4754
Marco Roth <marco.roth@intergga.ch>
2023-01-15 21:17:47 +0100

    Use `XPathToElement`, `XPathToArray`, `debounce` and `dispatch` from CableReady

--- javascript/utils.js
+++ javascript/utils.js
@@ -1,6 +1,7 @@
 import Schema from './schema'
 import Deprecate from './deprecate'
 import Debug from './debug'
+import { Utils } from 'cable_ready'
 
 // uuid4 function taken from stackoverflow
 // https://stackoverflow.com/a/2117523/554903
@@ -55,32 +56,16 @@ const camelize = (value, uppercaseFirstLetter = true) => {
   return value
 }
 
-const debounce = (callback, delay = 250) => {
-  let timeoutId
-  return (...args) => {
-    clearTimeout(timeoutId)
-    timeoutId = setTimeout(() => {
-      timeoutId = null
-      callback(...args)
-    }, delay)
-  }
-}
+// TODO: remove this in v4 (potentially!)
+const XPathToElement = Utils.xpathToElement
+const XPathToArray = Utils.xpathToElementArray
+const debounce = Utils.debounce
+const emitEvent = (name, detail = {}) => Utils.dispatch(document, name, detail)
 
 const extractReflexName = reflexString => {
   const match = reflexString.match(/(?:.*->)?(.*?)(?:Reflex)?#/)
 
   return match ? match[1] : ''
-}
-
-const emitEvent = (event, detail) => {
-  document.dispatchEvent(
-    new CustomEvent(event, {
-      bubbles: true,
-      cancelable: false,
-      detail
-    })
-  )
-  if (window.jQuery) window.jQuery(document).trigger(event, detail)
 }
 
 // construct a valid xPath for an element in the DOM
@@ -106,35 +91,6 @@ const elementToXPath = element => {
       ix++
     }
   }
-}
-
-// TODO: remove this in v4 (potentially!)
-const XPathToElement = xpath => {
-  return document.evaluate(
-    xpath,
-    document,
-    null,
-    XPathResult.FIRST_ORDERED_NODE_TYPE,
-    null
-  ).singleNodeValue
-}
-
-const XPathToArray = (xpath, reverse = false) => {
-  const snapshotList = document.evaluate(
-    xpath,
-    document,
-    null,
-    XPathResult.ORDERED_NODE_SNAPSHOT_TYPE,
-    null
-  )
-
-  const snapshots = []
-
-  for (let i = 0; i < snapshotList.snapshotLength; i++) {
-    snapshots.push(snapshotList.snapshotItem(i))
-  }
-
-  return reverse ? snapshots.reverse() : snapshots
 }
 
 const elementInvalid = element => {
