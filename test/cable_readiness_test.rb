6c65f2cc2375ae458af4850d645dc11fb0a5dee2
Marco Roth <marco.roth@intergga.ch>
2023-05-25 22:54:08 +0200

    standardize

--- test/cable_readiness_test.rb
+++ test/cable_readiness_test.rb
@@ -2,20 +2,20 @@
 
 require_relative "test_helper"
 
+class MyReflexPrepended
+  prepend StimulusReflex::CableReadiness
+
+  def id
+    "123"
+  end
+
+  def stream_name
+    "123"
+  end
+end
+
 class StimulusReflex::CableReadinessTest < ActiveSupport::TestCase
   test "can be prepended" do
-    class MyReflexPrepended
-      prepend StimulusReflex::CableReadiness
-
-      def id
-        "123"
-      end
-
-      def stream_name
-        "123"
-      end
-    end
-
     reflex = MyReflexPrepended.new
     assert_includes reflex.methods, :cable_ready
   end
