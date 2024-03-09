2924ee0dd4310eb27d34a8d695acc402b4bf4993
Matt-Yorkley <9029026+Matt-Yorkley@users.noreply.github.com>
2024-03-09 12:37:20 +0000

    standardrb --fix

--- lib/stimulus_reflex/instrumentation.rb
+++ lib/stimulus_reflex/instrumentation.rb
@@ -8,17 +8,17 @@ module StimulusReflex
       events = []
 
       time = Benchmark.realtime do
-        ActiveSupport::Notifications.subscribed(Proc.new{ |event| events << event }, /^sql.active_record|^render/) do
+        ActiveSupport::Notifications.subscribed(proc { |event| events << event }, /^sql.active_record|^render/) do
           yield
         end
       end
 
       sql, views = events.partition { |e| e.name.match?(/^sql/) }
 
-      reflex.logger.info "Processed #{reflex.class.name}##{reflex.method_name} in #{(time * 1000).round(1)}ms " +
-                           "(Views: #{views.sum(&:duration).round(1)}ms | " +
-                           "ActiveRecord: #{sql.sum(&:duration).round(1)}ms | " +
-                           "Allocations: #{events.sum(&:allocations)}) \n"
+      reflex.logger.info "Processed #{reflex.class.name}##{reflex.method_name} in #{(time * 1000).round(1)}ms " \
+        "(Views: #{views.sum(&:duration).round(1)}ms | " \
+        "ActiveRecord: #{sql.sum(&:duration).round(1)}ms | " \
+        "Allocations: #{events.sum(&:allocations)}) \n"
     end
   end
 end
