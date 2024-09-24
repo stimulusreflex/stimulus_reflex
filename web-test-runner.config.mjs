de2acf2a954a993fdfc57ce4732f84d43e4f2b1a
Marco Roth <marco.roth@intergga.ch>
2024-09-24 12:37:44 -0400

    Add `filterBrowserLogs` for `web-test-runner` config

--- web-test-runner.config.mjs
+++ web-test-runner.config.mjs
@@ -3,8 +3,22 @@ import { fromRollup } from '@web/dev-server-rollup'
 
 const json = fromRollup(rollupJson)
 
+const filteredLogs = [
+  'Lit is in dev mode. Not recommended for production! See https://lit.dev/msg/dev-mode for more information.'
+]
+
+const filterBrowserLogs = (log) => {
+  for (const arg of log.args) {
+    if (typeof arg === 'string' && filteredLogs.some(l => arg.includes(l))) {
+      return false
+    }
+  }
+  return true
+}
+
 export default {
   nodeResolve: true,
+  filterBrowserLogs,
   mimeTypes: {
     '**/*.json': 'js'
   },
